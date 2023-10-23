4c791503562eff8872b61037faef2d3a615889db
uun setiawati <uunsetiawati@gmail.com>
2023-10-23 15:13:46 +0700

    edit jenis produk dan flash alert

--- application/views/jenisproduk/data_jenisproduk.php
+++ application/views/jenisproduk/data_jenisproduk.php
@@ -22,11 +22,21 @@
             <div class="content">
 				<div class="container">
 					<div class="row">
-                        <div class="col-3">
-                            <?php
-                                echo anchor('jenisproduk/add', '<button class="button button-fill button-small">Tambah Data</button>');
-                            ?>
-                        </div>                                                                                      
+                        <div class="col-6">
+                            <div class="button-default">
+                                <?php
+                                    echo anchor('jenisproduk/add', '<button class="button">Tambah Data</button>');
+                                ?>
+                            </div>       
+                        </div>                                                                
+                        <div class="col-6">                           
+                            <div class="button-default">   
+                                <?php
+                                    echo anchor('dashboard', 'Kembali', array('class'=>'button2'));
+                                ?>
+                            </div>
+                        </div>
+
                     </div>
                     <div class="card">
 					<div class="card-body">
