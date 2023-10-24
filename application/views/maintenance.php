dbab7f6f5250fd1c66fc1abe0949c15f8f2dc3f3
uun setiawati <uunsetiawati@gmail.com>
2023-10-24 09:32:15 +0700

    tampilan maintenance

---
+++ application/views/maintenance.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="icon" href="https://bd.uptkukm.id/assets/images/favicon.png">
+	<title>Maxui</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700,900&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://bd.uptkukm.id/assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://bd.uptkukm.id/assets/css/ionicons.min.css">
+	<link rel="stylesheet" href="https://bd.uptkukm.id/assets/css/fakeLoader.css">
+	<link rel="stylesheet" href="https://bd.uptkukm.id/assets/css/swiper.min.css">
+	<link rel="stylesheet" href="https://bd.uptkukm.id/assets/css/style.css">
+
+</head>
+<body>
+	
+	<!-- fakeloader -->
+	<div class="fakeLoader"></div>
+	<!-- end fakeloader -->
+	
+	<!-- navbar -->
+	<div class="navbar">
+		<div class="left">
+			<a href="" class="link link-back"><i class="icon ion-ios-arrow-back"></i></a>
+		</div>
+		<div class="title">
+			Maintenance
+		</div>
+		<div class="right">
+			
+		</div>
+	</div>
+	<!-- end navbar -->
+
+	<!-- pages wrapper -->
+	<div class="pages-wrapper">
+
+		<!-- separator -->
+		<div class="separator-large"></div>
+		<!-- end separator -->
+		
+		<!-- maintenance -->
+		<div class="maintenance">
+			<div class="container">
+				<img src="https://bd.uptkukm.id/assets/images/maintenance.png" alt="">
+				<h5 class="mb-2">Website Under Maintenance</h5>
+				<p class="text-small">See You On Top | Sabar untuk membuka jalur langit</p>
+				<div class="button-default">
+					<div class="row">
+						<div class="col-6">
+							<a href="https://bd.uptkukm.id/dahsborad" class="button">Home</a>
+						</div>
+						<div class="col-6">
+							<a href="https://wa.me/6281331220006" class="button button-outline">Contact Us</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- end maintenance -->
+
+	</div>
+	<!-- end pages wrapper -->
+
+	<script src="https://bd.uptkukm.id/assets/js/jquery-3.4.1.min.js"></script>
+	<script src="https://bd.uptkukm.id/assets/js/bootstrap.bundle.min.js"></script>
+	<script src="https://bd.uptkukm.id/assets/js/fakeLoader.js"></script>
+	<script src="https://bd.uptkukm.id/assets/js/swiper.min.js"></script>
+	<script src="https://bd.uptkukm.id/assets/js/main.js"></script>
+
+</body>
+</html>
